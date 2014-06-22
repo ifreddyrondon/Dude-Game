2ef1ae9ad145b8d16b8d444721a6d79cd94a9d74
spantons <spantons@gmail.com>
2014-06-22 18:58:22 -0430

    Awakening dialogues lvl5

--- src/com/spantons/magicNumbers/StringDialogue.java
+++ src/com/spantons/magicNumbers/StringDialogue.java
@@ -13,12 +13,4 @@ public interface StringDialogue {
 		"Hay que buscar una salida",
 		"Debe haber una puerta en algún lado"};
 	
-	public static final String[] STORY_MAIN_ROOM = {
-		"¿Quién hizo esto?",
-		"Lo sabía",
-		"No tuve nada que ver con esto",
-		"Maldición ¿Quién fue?",
-		"¿Por qué lo hicieron?",
-		"Vamos a morir todos"};
-	
 }
